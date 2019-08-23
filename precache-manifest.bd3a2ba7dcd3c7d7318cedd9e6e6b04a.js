66130d66db65ffa00d3a7409c3ccea2ca37139f1
Dear Roy <royatrangpur@gmail.com>
2019-08-24 05:03:32 +0600

    Add files via upload

---
+++ precache-manifest.bd3a2ba7dcd3c7d7318cedd9e6e6b04a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5e4be24957e270c636bbfd7124fb88",
+    "url": "/apedin/index.html"
+  },
+  {
+    "revision": "a0c488689cabe1e9ff03",
+    "url": "/apedin/static/css/main.6b8625f1.chunk.css"
+  },
+  {
+    "revision": "a5d7326725610bfa5803",
+    "url": "/apedin/static/js/2.bdd85742.chunk.js"
+  },
+  {
+    "revision": "a0c488689cabe1e9ff03",
+    "url": "/apedin/static/js/main.4c74f2b3.chunk.js"
+  },
+  {
+    "revision": "071dbd4daf6869d94dbe",
+    "url": "/apedin/static/js/runtime~main.04c0e0f4.js"
+  },
+  {
+    "revision": "46a3f1f9b77f25bdf7e1f06506e91f51",
+    "url": "/apedin/static/media/logo.46a3f1f9.png"
+  }
+]);
