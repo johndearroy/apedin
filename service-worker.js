063f82c53e5d8e9077780bed46f92ec663cdf91b
Dear Roy <royatrangpur@gmail.com>
2019-09-23 02:04:49 +0600

    New Nav Deawer

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/apedin/precache-manifest.cb9140701303c18d9946476a1099c139.js"
+  "/apedin/precache-manifest.f31b39ecf12d0f33a32a0265d763eb97.js"
 );
 
 self.addEventListener('message', (event) => {
